824b1781feba0a7ed1af46a914e545ed307d4be4
harrssh18 <harshdubey8225@gmail.com>
2021-06-04 14:54:59 +0530

    final commit

--- calc/views.py
+++ calc/views.py
@@ -13,6 +13,9 @@ def lengthcontraction(request):
         sr = request.POST.get('sr')
         v = request.POST.get('v')
         c=299792458
+        def zerocount(v):
+            l = str(v).count('0')
+            return int(l)
         def meterconverter(lentype,lenn):
                 if lentype == 'km':
                     m = lenn * 1000
@@ -50,15 +53,34 @@ def lengthcontraction(request):
                     vm = velo 
                 y = math.sqrt(1 - (vm**2/c**2))
                 re = lm * y
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    startpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'m':re,'cm':re*100,'mm':re*1000,'km':re/1000,'in':re*39.37,'ft':re*3.28084,'yd':re*1.094,'mi':re/1609} 
                 context = {
+                'output': k1,
                 'redict':redict,
                 'y':y,
                 'lm':lm,
                 'vm':vm,
+                'result':re,
                 'l_op':lentype,
                 'v_op':veltype,
-                'result':re,
+                'start':startpoint,
+                'end':endpoint,
                 'Given':Given,
                 'l':lenn,
                 'v':velo,
@@ -75,9 +97,26 @@ def lengthcontraction(request):
                 # convert length to meters
                 lm = meterconverter(lentype,lenn)
                 re = srm / lm
-                redict = {'m/s':re,'km':re/1000,'mi/s':re*1609
-,'ligth speed':re/2.998e+8} 
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    startpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
+                redict = {'m/s':re,'km':re/1000,'mi/s':re*1609,'ligth speed':re/2.998e+8} 
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                 'redict':redict,
                 'srm':srm,
                 'lm':lm,
@@ -108,8 +147,26 @@ def lengthcontraction(request):
                 srm = meterconverter(srtype,srlen)
                 y = math.sqrt(1-vm**2/c**2)
                 re = srm / y
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    startpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'m':re,'cm':re*100,'mm':re*1000,'km':re/1000,'in':re*39.37,'ft':re*3.28084,'yd':re*1.094,'mi':re/1609} 
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                 'redict':redict,
                 'srm':srm,
                 'vm':vm,
@@ -133,7 +190,13 @@ def capacitance(request):
         a = request.POST.get('a')
         d = request.POST.get('d')
         c = request.POST.get('c')
-
+        def zerocount(v):
+            v = str(v)
+            c=0
+            for i in v:
+                if i == '0':
+                    c+=1
+            return c
         def squareconverter(t,l):
             if t == 'mm':
                 l = l / 0.000001
@@ -187,10 +250,31 @@ def capacitance(request):
                 e = 8.854
                 am = squareconverter(a_op,a)
                 dm = lengthconverter(d_op,d)       
-                c = (e*(am/dm))
-                c = c * 0.000000000001
+                c1 = (e*(am/dm))
+                c = c1 * 0.000000000001
+                print(c)
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(c)
+                if str(c) in 'e':
+                    index_num = c.index('e')
+                    startpoint = c[:index_num]
+                    endpoint = c[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(c))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
+
                 redict = {'Farad (F)':c,'MilliFarad (mF)':c*1000,'MicroFarad (uF)':c*1000000,'NanoFarad (nF)':c*10000000000,'PicoFarad (pF)':c*1000000000000}
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                 'a':a,
                 'd':d,
                 'e':e,
@@ -212,9 +296,29 @@ def capacitance(request):
                 cm = faradconverter(c_op,c)
                 am = squareconverter(a_op,a) 
                 d = 8.854 * .000000000001 * am / cm
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(d)
+                if str(d) in 'e':
+                    index_num = d.index('e')
+                    startpoint = d[:index_num]
+                    endpoint = d[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(d))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
+                    
                 redict = {'m':d,'mm':d*1000,'cm':d*100,'in':d*39.37,'ft':d*3.281,'yd':d*1.094
                 ,'mi':d/1609}
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                 'c':c,
                 'a':a,
                 'e':e,
@@ -236,9 +340,28 @@ def capacitance(request):
                 cm = faradconverter(c_op,c)
                 dm = lengthconverter(d_op,d)
                 a = (dm*cm)/(8.854*0.000000000001)
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(a)
+                if str(a) in 'e':
+                    index_num = a.index('e')
+                    startpoint = a[:index_num]
+                    endpoint = a[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(a))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'Sqaure Meters(m)':a,'Sqaure Millimeters(mm)':a*1000000,'Sqaure inches(in)':a*1550
             ,'Sqaure Yard(yd)':a*1.19599,'square feet(ft)':a*10.764}
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                 'c':c,
                 'd':d,
                 'e':e,
@@ -270,6 +393,9 @@ def cooling(request):
         t = request.POST.get('t')
 
         if request.method == "POST":
+            def zerocount(v):
+                l = str(v).count('0')
+                return int(l)
             def tempconverter(t,v):
                 if t=='f':
                     c = (v-32) * 5/9
@@ -309,8 +435,28 @@ def cooling(request):
                 tps = timeconverter(tp_op,tp)
                 ks = powertimeconverter(k_op,k)
                 re = atc + (itc - atc) * math.exp(-ks*(tps))
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    stratpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'°C':re,'°F':(re * 9/5) + 32 ,'K': re + 273.15}
-                context = {'Given':Given,
+                context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
+                'Given':Given,
                 'it':it,
                 'at':at,
                 'k':k,
@@ -344,8 +490,29 @@ def cooling(request):
                 ks = powertimeconverter(k_op,k)
                 
                 re = tc - itc * math.exp(-ks*tps) 
+
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    stratpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'°C':re,'°F':(re * 9/5) + 32 ,'K': re + 273.15}
-                context = {'Given':Given,
+                context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
+                'Given':Given,
                 'it':it,
                 't':t,
                 'k':k,
@@ -378,8 +545,28 @@ def cooling(request):
                 tps = timeconverter(tp_op,tp)
                 ks = powertimeconverter(k_op,k)
                 re = atc + (tc-atc) * math.exp(-ks*(tps))
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    stratpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'°C':re,'°F':(re * 9/5) + 32 ,'K': re + 273.15}
-                context = {'Given':Given,
+                context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
+                'Given':Given,
                 'at':at,
                 't':t,
                 'k':k,
@@ -414,9 +601,28 @@ def cooling(request):
                 tps = timeconverter(tp_op,tp)
                 re1 = (itc-atc)/(tc-atc)
                 re = math.log(re1)/tps
-                
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    stratpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'Sec⁻¹':re,'Min⁻¹':re / 0.016667 ,'Hrs⁻¹': re / 0.0002778}
-                context = {'Given':Given,
+                context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
+                'Given':Given,
                 'at':at,
                 't':t,
                 'it':it,
@@ -452,8 +658,28 @@ def cooling(request):
                 ks = powertimeconverter(k_op,k)
                 re1 = (itc-atc)/(tc-atc)
                 re = math.log(re1)/ks
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(re)
+                if str(re) in 'e':
+                    index_num = re.index('e')
+                    stratpoint = re[:index_num]
+                    endpoint = re[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(re))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'Sec':re,'Min':re / 60 ,'Hrs': re / 3600}
-                context = {'Given':Given,
+                context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
+                'Given':Given,
                 'at':at,
                 't':t,
                 'it':it,
@@ -486,7 +712,9 @@ def thermalconduct(request):
         t = request.POST.get('t')
         q = request.POST.get('q')
         d = request.POST.get('d')
-        
+        def zerocount(v):
+            l = str(v).count('0')
+            return int(l)
         def lengthconverter(t,l):
             if t == 'mm':
                 l = l / 1000
@@ -539,7 +767,26 @@ def thermalconduct(request):
                 tm = tempconverter(t_op,t)
         
                 q = -(tc*tm)/dm
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(q)
+                if str(q) in 'e':
+                    index_num = q.index('e')
+                    stratpoint = q[:index_num]
+                    endpoint = q[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(q))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 context = {
+                    'output':k1,
+                    'start':startpoint,
+                    'end':endpoint,
                     't':t,
                     'd':d,
                     'tm':tm,
@@ -560,9 +807,28 @@ def thermalconduct(request):
                 #convert distance on meters
                 tm = tempconverter(t_op,t)
                 d = -(tc*tm)/q
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(d)
+                if str(d) in 'e':
+                    index_num = d.index('e')
+                    stratpoint = d[:index_num]
+                    endpoint = d[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(d))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'m':d,'mm':d*1000,'cm':d*100,'km':d/1000,'mi':d/1609,'in':d*39.37,'ft':d*3.281
     ,'yd':d* 1.094}
                 context = {
+                    'output':k1,
+                    'start':startpoint,
+                    'end':endpoint,
                     't':t,
                     'tc':tc,
                     'q':q,
@@ -583,7 +849,26 @@ def thermalconduct(request):
                 dm = lengthconverter(d_op,d)
                 tm = tempconverter(t_op,t)
                 tc = -(q*dm)/tm
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(tc)
+                if str(tc) in 'e':
+                    index_num = tc.index('e')
+                    stratpoint = tc[:index_num]
+                    endpoint = tc[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(tc))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 context = {
+                    'output':k1,
+                    'start':startpoint,
+                    'end':endpoint,
                     't':t,
                     'd':d,
                     'q':q,
@@ -604,9 +889,28 @@ def thermalconduct(request):
                 #convert distance on meters
                 dm = lengthconverter(d_op,d)
                 t = (q*dm)/-tc
+                k1 = False
+                index_num = 0
+                startpoint = 0
+                endpoint = 0
+                count = zerocount(t)
+                if str(t) in 'e':
+                    index_num = t.index('e')
+                    stratpoint = t[:index_num]
+                    endpoint = t[index_num+1:]
+                    k1 = True
+                elif count>5:
+                    r11 = str("{:.2e}".format(t))
+                    index_num = r11.index('e')
+                    startpoint = r11[:index_num]
+                    endpoint = r11[index_num+1:]
+                    k1 = True
                 redict = {'°C':t,'°F':(t * 9/5) + 32,'K':t+ 273.15,'°R':t*9/5 + 491.67,'°De':( 100 - t ) * 3 / 2,'°N':t* 0.33000,'°Re':t* 4/5
     ,'°Ro':t* 21/40 + 7.5}
                 context = {
+                'output':k1,
+                'start':startpoint,
+                'end':endpoint,
                     'dm':dm,
                     'd':d,
                     'tc':tc,
